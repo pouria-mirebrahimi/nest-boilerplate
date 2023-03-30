2a62f368987907f80c9f41c140e544ffb596764e
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2023-03-31 00:53:28 +0330

    Added task scheduling
    https://docs.nestjs.com/techniques/task-scheduling

--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,17 +1,22 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateUserDto } from '../dto/user.dto';
+// entity
 import { User } from '../entity/user.entity';
+import { UserView } from '../view/user.view';
+// repository
 import { UserRepository } from '../repository/user.repository';
 import { UserViewRepository } from '../repository/user.view.repository';
-import { UserView } from '../view/user.view';
+// dto
+import { CreateUserDto } from '../dto/user.dto';
 
 @Injectable()
 export class UserService {
-  @InjectRepository(UserRepository)
-  private readonly repository: UserRepository;
-  @InjectRepository(UserViewRepository)
-  private readonly userViewRepo: UserViewRepository;
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly repository: UserRepository,
+    @InjectRepository(UserViewRepository)
+    private readonly userViewRepo: UserViewRepository,
+  ) {}
 
   async fetchAllUsers(): Promise<User[]> {
     return await this.repository.queryAllUsersAndCountRoles();
