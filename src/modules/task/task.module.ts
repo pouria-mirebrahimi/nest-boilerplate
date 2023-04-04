6b668fb30dd80885602d2308cd9c95894f186f13
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2023-04-04 15:11:50 +0330

    Modified module name
    Task is more appropriate

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 // service
-import { TaskService } from './service/tasks.service';
+import { TaskService } from './service/task.service';
 
 @Module({
   providers: [TaskService],
