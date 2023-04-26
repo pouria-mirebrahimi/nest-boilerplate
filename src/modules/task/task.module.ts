bb9ce7128bd699c8cda18427bee3097019aca9fd
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2023-04-26 15:45:45 +0330

    Adding Global decorator to use a module globally

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -4,6 +4,11 @@ import { UserModule } from '../user/user.module';
 // service
 import { TaskService } from './service/task.service';
 
+/**
+ * @description this is for when a module must be imported in the
+ *              app module once and be used everywhere in the project.
+ */
+// @Global()
 @Module({
   imports: [UserModule],
   providers: [TaskService],
